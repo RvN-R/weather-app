71d232f110b9755ece6501af6c0b7cc8ff2939e1
Richard Van Nairn <rvannairn@googlemail.com>
2022-07-24 15:31:41 +0100

    Created current weather component

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "254f908999mshaec63a0df1091bcp10913bjsn0389dfea9aa8",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8eda8d2e55c4584c4e18d54ba1eba5d6";
